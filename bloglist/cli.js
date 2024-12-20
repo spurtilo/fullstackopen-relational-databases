dbc09beaba7e7b1ba8c98d8c04944084b017f866
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-12-20 08:56:49 +0200

    Refactor cli.js

--- bloglist/cli.js
+++ bloglist/cli.js
@@ -3,16 +3,16 @@ const sequelize = require('./db');
 
 const printBlogs = async () => {
   try {
-    await sequelize.authenticate();
     const blogs = await sequelize.query('SELECT * FROM blogs', {
-      type: QueryTypes,
+      type: QueryTypes.SELECT,
     });
     blogs.forEach((b) =>
       console.log(`${b.author}: '${b.title}', ${b.likes} likes`)
     );
-    sequelize.close();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+  } finally {
+    sequelize.close();
   }
 };
 
