36e6d6d2cd6fc1204059023428986f7bc36ce904
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-12-20 09:48:42 +0200

    Code along with note app

--- notes/index.js
+++ notes/index.js
@@ -43,9 +43,24 @@ Note.init(
   }
 );
 
+Note.sync();
+
 app.get('/api/notes', async (req, res) => {
   const notes = await Note.findAll();
+
+  console.log(JSON.stringify(notes, null, 2));
+
   res.json(notes);
+});
+
+app.get('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    console.log(note.toJSON());
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.post('/api/notes', async (req, res) => {
@@ -54,6 +69,17 @@ app.post('/api/notes', async (req, res) => {
     return res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.put('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
 });
 
