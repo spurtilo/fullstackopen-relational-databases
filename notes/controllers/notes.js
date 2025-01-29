1d28ad83c1c3bd8a984616242b2eef4237300cf0
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2025-01-29 18:22:50 +0200

    Code along

--- notes/controllers/notes.js
+++ notes/controllers/notes.js
@@ -1,25 +1,8 @@
 const router = require('express').Router();
 const { Op } = require('sequelize');
-const jwt = require('jsonwebtoken');
-const { SECRET } = require('../util/config');
 
 const { User, Note } = require('../models');
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      console.log(authorization.substring(7));
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch (error) {
-      console.log(error);
-      return res.status(401).json({ error: 'token invalid' });
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' });
-  }
-  next();
-};
+const { tokenExtractor } = require('../util/middleware');
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
