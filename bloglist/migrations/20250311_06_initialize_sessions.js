551793992131312822b483a44c42231da75c5c75
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2025-03-11 14:54:00 +0200

    Initialize sessions table to database

---
+++ bloglist/migrations/20250311_06_initialize_sessions.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('sessions', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+          isInt: { msg: 'The id field must be an integer' },
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('sessions');
+  },
+};
