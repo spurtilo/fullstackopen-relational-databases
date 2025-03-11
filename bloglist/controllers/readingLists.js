3a0ce2950931a9cc95371d4df4bb3ec5ea7731ac
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2025-03-11 11:26:31 +0200

    Implement marking blogs as read in reading list

--- bloglist/controllers/readingLists.js
+++ bloglist/controllers/readingLists.js
@@ -16,4 +16,24 @@ readingRouter.post('/', middleware.userExtractor, async (req, res) => {
   }
 });
 
+readingRouter.put('/:id', middleware.userExtractor, async (req, res) => {
+  const readingListEntry = await ReadingList.findByPk(req.params.id);
+
+  if (readingListEntry && readingListEntry.userId === req.user.id) {
+    if (readingListEntry.isRead) {
+      res.status(400).json({ error: 'This blog is already marked as read' });
+      return;
+    }
+
+    readingListEntry.isRead = true;
+    await readingListEntry.save();
+
+    res.status(200).json(readingListEntry);
+  } else {
+    res
+      .status(403)
+      .json({ error: 'Only blogs on your reading list can be marked as read' });
+  }
+});
+
 module.exports = readingRouter;
